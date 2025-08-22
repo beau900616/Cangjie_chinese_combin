2d1190886edef97fadf6c2a59ed16a2fa848456e
beau900616 <beau_wang@kimo.com>
2025-08-22 12:03:19 +0800

    add right side bar

--- static/script.js
+++ static/script.js
@@ -3,9 +3,12 @@ const ctx = canvas.getContext("2d");
 const sidebar = document.getElementById("sidebar");
 const input = document.getElementById("newCharInput");
 const addBtn = document.getElementById("addBtn");
+const biggerBtn = document.getElementById("bigger");
+const smallerBtn = document.getElementById("smaller");
 
 let selectedChar = null;   // 當前選中的字
 let placedChars = [];      // 已放置的字方塊
+let frontsize = 32;      // 當前的文字大小
 
 // 側邊選單點擊
 document.querySelectorAll(".char-btn").forEach(btn => {
@@ -39,6 +42,24 @@ addBtn.addEventListener("click", () => {
   }
 });
 
+biggerBtn.addEventListener("click", () => {
+  if (frontsize <= 100) {
+    frontsize = frontsize + 1
+    updateFontsizeDisplay()
+  }
+});
+
+smallerBtn.addEventListener("click", () => {
+  if (frontsize >= 10) {
+    frontsize = frontsize - 1
+    updateFontsizeDisplay()
+  }
+});
+
+function updateFontsizeDisplay() {
+  document.getElementById("fontsize-display").innerText = `字體大小：${frontsize}px`;
+}
+
 // 滑鼠移動 → 顯示跟隨的字
 canvas.addEventListener("mousemove", (e) => {
   if (!selectedChar) return;
@@ -50,11 +71,20 @@ canvas.addEventListener("mousemove", (e) => {
   redrawCanvas();
 
   // 畫跟隨的字（半透明）
-  ctx.globalAlpha = 0.5;
-  ctx.font = "32px Microsoft JhengHei";
+  ctx.globalAlpha = 0.6;
+  ctx.font = frontsize + "px Microsoft JhengHei";
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
-  ctx.fillText(selectedChar, x, y);
+  if (selectedChar == '橡皮擦'){
+    // 畫白色矩形遮住
+    ctx.fillStyle = "white";
+    // 這裡設一個矩形大小，例如 40x40，可依需求調整
+    ctx.fillRect(x - 20, y - 20, 40, 40);
+  }
+  else {
+    ctx.fillStyle = "black"; // 正常文字黑色
+    ctx.fillText(selectedChar, x, y);
+  };
   ctx.globalAlpha = 1.0;
 });
 
@@ -79,6 +109,16 @@ function redrawCanvas() {
   ctx.textBaseline = "middle";
 
   placedChars.forEach(item => {
-    ctx.fillText(item.char, item.x, item.y);
+    if (item.char == '橡皮擦'){
+      // 畫白色矩形遮住
+      ctx.fillStyle = "white";
+      // 這裡設一個矩形大小，例如 40x40，可依需求調整
+      ctx.fillRect(item.x - 20, item.y - 20, 40, 40);
+    }
+    else {
+      ctx.fillStyle = "black"; // 正常文字黑色
+      ctx.fillText(item.char, item.x, item.y);
+    };
+    
   });
 }
